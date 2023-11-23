224bd9d3429546e387079812c42a30e19c6f79eb
sladeberryman <slade12berryman@gmail.com>
2023-11-23 10:10:04 -0600

    Final template

--- scripts/resume.js
+++ scripts/resume.js
@@ -7,24 +7,26 @@ $(document).ready(function() {
         html += '<h1>' + data.name + '</h1>';
 
         // Contact Information
-        html += '<div class="resume-section">';
+        html += '<div class="resume-section top-header">';
         html += '<p>' + data.email + ' | ' + data.phone + '</p>';
+        html += '<a href="' + data.linkedIn + '">' + data.linkedIn + '</a>';
         html += '</div>';
         html += '<hr>';
 
         // Education
-        html += '<h3>Education:</h3>';
+        html += '<h3>Education</h3>';
         for (var i = 0; i < data.education.length; i++) {
         html += '<div class="resume-section">';
         html += '<h4>' + data.education[i].degree + '</h4>';
         html += '<p>' + data.education[i].university + '</p>';
-        html += '<p>' + data.education[i].year + '</p>';
+        html += '<p>' + data.education[i].startYear + '-' + data.education[i].endYear + '</p>';
+        html += '<p>' + data.education[i].gpa + ' GPA</p>';
         html += '</div>';
         }
-        html += '<hr>';
+        html += '<br>';
 
         // Work Experience
-        html += '<h3>Work Experience:</h3>';
+        html += '<h3>Work Experience</h3>';
         for (var j = 0; j < data.experience.length; j++) {
         html += '<div class="resume-section">';
         html += '<h4>' + data.experience[j].position + '</h4>';
@@ -33,15 +35,15 @@ $(document).ready(function() {
         html += '<p>' + data.experience[j].description + '</p>';
         html += '</div>';
         }
-        html += '<hr>';
+        html += '<br>';
 
         // Skills
-        html += '<h3>Skills:</h3>';
+        html += '<h3>Skills</h3>';
         html += '<ul>';
         for (var k = 0; k < data.skills.length; k++) {
         html += '<li>' + data.skills[k] + '</li>';
         }
-        html += '</ulr>';
+        html += '</ul>';
 
 
         resumeData.html(html);
